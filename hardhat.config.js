3d6bab62fb26a61d38a10cecc2cd15cf9c040320
nftlegends team <info@nftlegends.io>
2021-05-11 13:08:07 +0300

    NFT-121 Upd hardhat config and deps

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,71 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-truffle5');
+require('@nomiclabs/hardhat-solhint');
+require('@nomiclabs/hardhat-etherscan');
+require('solidity-coverage');
+require('hardhat-deploy')
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+const accounts = {
+  mnemonic: process.env.MNEMONIC || "test test test test test test test test test test test junk"
+}
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.7.3",
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    deploy: "deploy",
+    deployments: "deployments",
+    imports: "imports",
+    sources: "contracts",
+    tests: "test",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        enabled: process.env.FORKING === "true",
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+      live: false,
+      saveDeployments: false,
+      tags: ["test"],
+    },
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
+    },
+    rinkeby: {
+      url: `wss://rinkeby.infura.io/ws/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 3,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+    },
+    mainnet: {
+      url: `wss://mainnet.infura.io/ws/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 1,
+      live: true,
+      saveDeployments: true,
+      tags: ["production"],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
 };
-
