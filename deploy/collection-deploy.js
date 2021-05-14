0d8470f260fd771f8483203702645eec4bc233ed
nftlegends team <info@nftlegends.io>
2021-05-14 15:27:04 +0300

    NFT-120 Fix collection local deployments

--- deploy/collection-deploy.js
+++ deploy/collection-deploy.js
@@ -1,9 +1,13 @@
 module.exports = async ({
+  ethers,
   getNamedAccounts,
   deployments,
+  getChainId,
 }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+
+  const chainId = await getChainId()
 
   await deploy('Collection', {
     from: deployer,
@@ -11,4 +15,10 @@ module.exports = async ({
     args: [],
     log: true,
   });
+
+  const collection = await ethers.getContract('Collection');
+
+  if (chainId === '31337' || chainId === '1337') {
+    await collection.addSaleStage('1000', '1000');
+  }
 };
